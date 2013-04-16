e3b7bba71a0931d20fa76f4643fc4c70ab6ce316
FichteFoll <fichtefoll2@googlemail.com>
2013-04-16 12:13:09 +0300

    Update find selector and make auto-folding an option.
    The longer selector does not match normal strings and should reduce
    the overhead. Also make auto-folding optional by considering a
    "fold_python_docstrings_onload" setting which can be set in
    "User/Preferences.sublime-settings".
    
    Also do not fold the last char of the first line

--- foldpythondocstrings.py
+++ foldpythondocstrings.py
@@ -3,19 +3,20 @@ import sublime_plugin
 
 
 def fold_comments(view):
-    for region in view.find_by_selector('string'):
+    for region in view.find_by_selector('string.quoted.double.block, string.quoted.single.block'):
         lines = view.lines(region)
         if len(lines) > 1:
             region = sublime.Region(lines[0].begin(), lines[-1].end())
             text = view.substr(region).strip()
             if text.startswith("'''") or text.startswith('"""'):
-                fold_region = sublime.Region(lines[0].end() - 1, lines[-1].end() - 3)
+                fold_region = sublime.Region(lines[0].end(), lines[-1].end() - 3)
                 view.fold(fold_region)
 
 
 class FoldFilePythonDocstrings(sublime_plugin.EventListener):
     def on_load(self, view):
-        fold_comments(view)
+        if view.settings().get("fold_python_docstrings_onload", True):
+            fold_comments(view)
 
 
 class FoldPythonDocstringsCommand(sublime_plugin.TextCommand):
