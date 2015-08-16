77749eae662d3bf286893505729d7897c7eaecaa
Pradyun <pradyunsg@users.noreply.github.com>
2015-08-16 10:57:04 +0530

    Readablity changes to the Loop
    Moved certain declarations to the start of fold_comments for readability. Changed the loop conditions for better readability.

--- foldpythondocstrings.py
+++ foldpythondocstrings.py
@@ -3,15 +3,28 @@ import sublime_plugin
 
 
 def fold_comments(view):
-    number_lines_to_fold = view.settings().get("fold_python_docstrings_number_of_lines", 1)
-    for region in view.find_by_selector('string.quoted.double.block, string.quoted.single.block'):
+    # Selector moved out for readability
+    selectors = 'string.quoted.double.block, string.quoted.single.block'
+    number_lines_to_fold = view.settings().get(
+        "fold_python_docstrings_number_of_lines", 1
+    )
+
+    for region in view.find_by_selector(selectors):
         lines = view.lines(region)
-        if len(lines) > 1:
-            region = sublime.Region(lines[0].begin(), lines[-1].end())
-            text = view.substr(region).strip()
-            if text.startswith("'''") or text.startswith('"""'):
-                fold_region = sublime.Region(lines[number_lines_to_fold-1].end(), lines[-1].end() - 3)
-                view.fold(fold_region)
+        if len(lines) <= 1:
+            continue
+
+        region = sublime.Region(lines[0].begin(), lines[-1].end())
+        text = view.substr(region).strip()
+        if not text.startswith(("'''", '"""')):
+            continue
+
+        # Moved out for readability
+        a = lines[number_lines_to_fold - 1].end()
+        b = lines[-1].end() - 3
+
+        fold_region = sublime.Region(a, b)
+        view.fold(fold_region)
 
 
 class FoldFilePythonDocstrings(sublime_plugin.EventListener):
