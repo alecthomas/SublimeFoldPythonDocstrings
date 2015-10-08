522f00a618cbd60aad337d64527bdf5572d3bf12
Alec Thomas <alec@swapoff.org>
2015-10-08 13:34:16 +1100

    Support docstrings with just quotes on the first line.

--- foldpythondocstrings.py
+++ foldpythondocstrings.py
@@ -3,28 +3,46 @@ import sublime_plugin
 
 
 def fold_comments(view):
-    number_lines_to_fold = view.settings().get("fold_python_docstrings_number_of_lines", 1)
+    number_lines_to_fold = view.settings().get('fold_python_docstrings_number_of_lines', 1)
     for region in view.find_by_selector('string.quoted.double.block, string.quoted.single.block'):
         lines = view.lines(region)
-        if len(lines) > 1:
-            region = sublime.Region(lines[0].begin(), lines[-1].end())
-            text = view.substr(region).strip()
-            if text.startswith("'''") or text.startswith('"""'):
-                fold_region = sublime.Region(lines[number_lines_to_fold-1].end(), lines[-1].end() - 3)
-                view.fold(fold_region)
+        if len(lines) <= 1:
+            continue
+
+        region = sublime.Region(lines[0].begin(), lines[-1].end())
+        text = view.substr(region).strip()
+
+        if not (text.startswith("'''") or text.startswith('"""')):
+            continue
+
+        # Handle docstrings with just quotes on the first line.
+        if text[3:4] in '\n\r':
+            leading = len(text[4:]) - len(text[4:].lstrip())
+            region = sublime.Region(lines[0].end(), lines[1].begin() + leading)
+            view.fold(region)
+            start = 1
+        else:
+            start = 0
+
+        fold_region = sublime.Region(
+            lines[start+number_lines_to_fold-1].end(), lines[-1].end() - 3)
+        view.fold(fold_region)
 
 
 class FoldFilePythonDocstrings(sublime_plugin.EventListener):
+
     def on_load(self, view):
-        if view.settings().get("fold_python_docstrings_onload", True):
+        if view.settings().get('fold_python_docstrings_onload', True):
             fold_comments(view)
 
 
 class FoldPythonDocstringsCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         fold_comments(self.view)
 
 
 class UnfoldPythonDocstringsCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         self.view.unfold(self.view.find_by_selector('string'))
